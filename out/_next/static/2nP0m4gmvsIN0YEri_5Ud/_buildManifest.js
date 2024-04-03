774f3a5fe079442d01395d75d80aba757a248370
Prachaya Sinsuwan <sinsuwan@us.ibm.com>
2024-04-03 15:22:58 -0400

    feat(build) update LS

---
+++ out/_next/static/2nP0m4gmvsIN0YEri_5Ud/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1c920f04-c5632912fd7431b9.js","static/chunks/pages/index-0335fbe826d9632d.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
